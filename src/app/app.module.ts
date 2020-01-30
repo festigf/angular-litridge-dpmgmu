793a6d3550a490c29df692c539559fff14d8eea1
festigf <gianfranco.festi@marconirovereto.it>
2020-01-30 12:49:31 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {DataService} from './services/data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './admin/login/login.component';
+//import { LoginComponent } from './admin/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -15,7 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   declarations: [
     AppComponent,
     MainNavComponent,
-    LoginComponent,
+  //  LoginComponent,
     HomeComponent,
     PageNotFoundComponent
   ],
