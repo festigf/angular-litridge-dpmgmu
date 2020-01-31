0827537a5f70f146677060af70b341d98463c8ec
gianfranco.festi <gianfranco.festi@marconirovereto.it>
2020-01-31 05:58:22 +0100

    ini

--- src/app/departments/departments.module.ts
+++ src/app/departments/departments.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
-import { ListUtentiComponent } from './utenti-dashboard/list-utenti/list-utenti.component';
+import {ListDepartmentsComponent} from './departments/list-departments/list-departments.component';
 import { DepartmentsRoutingModule } from './departments-routing.module';
 import {DepartmentsComponent} from './departments/departments.component';
 
 
 @NgModule({
-  declarations: [DepartmentsComponent,ListUtentiComponent],
+  declarations: [DepartmentsComponent,ListDepartmentsComponent],
   imports: [
     CommonModule,
     DepartmentsRoutingModule,SharedModule
